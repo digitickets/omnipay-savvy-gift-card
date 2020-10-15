4f515f7dcc08c2490ea7cc86f9ae45c66b86f258
GlennJenkins <glenn.jenkins@digitickets.co.uk>
2020-10-15 12:59:08 +0100

    Make a start on the gateway class.

---
+++ src/SavvyGateway.php
@@ -0,0 +1,86 @@
+<?php
+
+/**
+ * Class SavvyGateway
+ *
+ * @method RequestInterface completeAuthorize(array $options = array()) (Optional method)
+ *         Handle return from off-site gateways after authorization
+ * @method RequestInterface capture(array $options = array())           (Optional method)
+ *         Capture an amount you have previously authorized
+ * @method RequestInterface completePurchase(array $options = array())  (Optional method)
+ *         Handle return from off-site gateways after purchase
+ * @method RequestInterface void(array $options = array())              (Optional method)
+ *         Generally can only be called up to 24 hours after submitting a transaction
+ * @method RequestInterface createCard(array $options = array())        (Optional method)
+ *         The returned response object includes a cardReference, which can be used for future transactions
+ * @method RequestInterface updateCard(array $options = array())        (Optional method)
+ *         Update a stored card
+ * @method RequestInterface deleteCard(array $options = array())        (Optional method)
+ *         Delete a stored card
+ */
+class SavvyGateway extends \DigiTickets\OmnipayAbstractVoucher\AbstractVoucherGateway
+{
+    public function getName()
+    {
+        return 'Savvy Gift Card';
+    }
+
+    /**
+     * This is just a wrapper around the parent createRequest() and just adds an instance of this gateway to the parameters.
+     *
+     * @param string $class
+     * @param array $parameters
+     *
+     * @return \Omnipay\Common\Message\AbstractRequest
+     */
+    protected function createRequest($class, array $parameters)
+    {
+        $parameters['gateway'] = $this;
+
+        return parent::createRequest($class, $parameters);
+    }
+
+    // @TODO: All the requests and responses need to be build for the below methods to work.
+//    public function authorize(array $parameters = array())
+//    {
+//        return $this->createRequest(AuthorizeRequest::class, $parameters);
+//    }
+//
+//    public function purchase(array $parameters = array())
+//    {
+//        return $this->createRequest(PurchaseRequest::class, $parameters);
+//    }
+//
+//    public function refund(array $parameters = array())
+//    {
+//        $parameters['unredeemRequest'] = $this->unredeem($parameters);
+//        return $this->createRequest(RefundRequest::class, $parameters);
+//    }
+
+//    /**
+//     * @param array $parameters
+//     * @return AbstractRequest
+//     */
+//    public function validate(array $parameters = array())
+//    {
+//        return $this->createRequest(ValidateRequest::class, $parameters);
+//    }
+//
+//    /**
+//     * @param array $parameters
+//     * @return AbstractRequest
+//     */
+//    public function redeem(array $parameters = array())
+//    {
+//        return $this->createRequest(RedeemRequest::class, $parameters);
+//    }
+//
+//    /**
+//     * @param array $parameters
+//     * @return AbstractRequest
+//     */
+//    public function unredeem(array $parameters = array())
+//    {
+//        return $this->createRequest(UnredeemRequest::class, $parameters);
+//    }
+}
